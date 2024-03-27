d0274e94c3a35eb759a65cf075590be307da68c9
Mary16RB <162063615+Mary16RB@users.noreply.github.com>
2024-03-27 13:20:53 -0600

    change9

--- comeApple/index.js
+++ comeApple/index.js
@@ -444,7 +444,7 @@ function update(){
 
   if(((this.cursors.space.isDown)&&(this.cont<1))||(mouseIsPressed &&(this.cont<1))){
     this.piggie.anims.play(salto);
-    this.piggie.y=300;
+    this.piggie.y=310;
     this.piggie.setVelocityY(-400);
     console.log(this.piggie.y);
     this.cont=1;
@@ -457,7 +457,7 @@ if(((this.cursors.space.isDown)&&(this.cont<=1)&&(this.piggie.y<280))||(mouseIsP
   this.cont=2;
   this.piggie.anims.play(salto);
 }
-if((this.piggie.y>=320)){
+if((this.piggie.y>=330)){
  this.piggie.anims.play(nivel,true); 
  this.cont=0;
 }
