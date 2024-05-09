f83b9829d98cc9ff4754087a02c5310ca133bfeb
Mary16RB <162063615+Mary16RB@users.noreply.github.com>
2024-05-09 11:11:44 -0600

    rankin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './Scripts/LogIn.js'
 import{ID} from './Scripts/LogIn.js'
 import './Scripts/LogOut.js'
 import { db } from './Scripts/firebase.js';
-import { doc, collection, setDoc, getDoc} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import { doc, collection, setDoc, getDoc, getDocs ,query, orderBy, limit} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import './Scripts/firebase_realtime.js'
 
 
@@ -16,6 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const nav = document.querySelector(".navega");
     const box = document.querySelector(".box");
     const Inicio= document.querySelector("#logo");
+    const Playgame = document.querySelector("#apple_game");
+
+    const SeccionRank= document.querySelector("body");
 
     const BTsoon = document.querySelector("#soon");
     const BThome = document.querySelector("#home_btn");
@@ -42,8 +45,43 @@ document.addEventListener("DOMContentLoaded", function() {
     const passOff = document.querySelector(".passOff");
     const passOn = document.querySelector(".passOn");
     const Pasword = document.querySelector(".cont-pass");
+
+    const Primero = document.querySelector("#primer");
+    const Segundo = document.querySelector("#second");
+    const Tersero = document.querySelector("#third");
+    const Cuarto = document.querySelector("#cuarto");
+    const Quinto = document.querySelector("#quinto");
+    const Sexto = document.querySelector("#sexto");
+    const Septimo = document.querySelector("#setimo");
+    const Octavo = document.querySelector("#octavo");
+    const Noveno = document.querySelector("#noveno");
+    const Decimo = document.querySelector("#decimo");
+    const Decimo_11 = document.querySelector("#decimo_primero");
+    const Decimo_12 = document.querySelector("#decimo_segundo");
+    const Decimo_13 = document.querySelector("#decimo_tercero");
+    const Decimo_14 = document.querySelector("#decimo-cuarto");
+    const Decimo_15 = document.querySelector("#decimo_quito");
+
+    const Rank1 = document.querySelector("#rank_1");
+    const Rank2 = document.querySelector("#rank_2");
+    const Rank3 = document.querySelector("#rank_3");
+    const Rank4 = document.querySelector("#rank_4");
+    const Rank5 = document.querySelector("#rank_5");
+    const Rank6 = document.querySelector("#rank_6");
+    const Rank7 = document.querySelector("#rank_7");
+    const Rank8 = document.querySelector("#rank_8");
+    const Rank9 = document.querySelector("#rank_9");
+    const Rank10 = document.querySelector("#rank_10");
+    const Rank11 = document.querySelector("#rank_11");
+    const Rank12 = document.querySelector("#rank_12");
+    const Rank13 = document.querySelector("#rank_13");
+    const Rank14 = document.querySelector("#rank_14");
+    const Rank15 = document.querySelector("#rank_15");
      
+    var cont=0; 
     var Puntaje;
+    let totalRankN= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    let totalRankS= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
     
     window.addEventListener('storage', async (event) => {
         if (event.key != 'Puntaje') return;
@@ -147,6 +185,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     logOut.addEventListener("click", () => {
         nav.classList.remove("press_Sign");
+        SeccionRank.classList.remove("Play_rank");
         
      
     });
@@ -157,6 +196,7 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("press_game");
         nav.classList.remove("press");
         nav.classList.remove("press_rank");
+        SeccionRank.classList.remove("Play_rank");
 
     });
 
@@ -166,7 +206,60 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("press_home");
         nav.classList.remove("press_game");
         nav.classList.remove("press");
+
+        Playgame.classList.remove("play");
+        home.classList.remove("play");
+
         nav.classList.add("press_rank");
+        SeccionRank.classList.add("Play_rank");
+
+        const scoreRef =collection(db, "users");
+        const q = query(scoreRef, orderBy("score","desc"), limit(15));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            let totalScore= parseInt(doc.data().score);
+            let rankName= doc.data().avatar;
+
+        console.log(cont);
+        totalRankN.splice(cont, 15, rankName.toString()); 
+        totalRankS.splice( cont, 15, totalScore);
+        cont++;
+});
+cont=0;
+     console.log(totalRankN,totalRankS);
+
+  Primero.innerHTML= (totalRankN[0]); 
+   Segundo.innerHTML= (totalRankN[1]);
+   Tersero.innerHTML= (totalRankN[2]);
+   Cuarto.innerHTML= (totalRankN[3]);
+   Quinto.innerHTML= (totalRankN[4]);
+   Sexto.innerHTML= (totalRankN[5]);
+   Septimo.innerHTML= (totalRankN[6]);
+   Octavo.innerHTML= (totalRankN[7]);
+   Noveno.innerHTML= (totalRankN[8]);
+   Decimo.innerHTML= (totalRankN[9]);
+   Decimo_11.innerHTML= (totalRankN[10]);
+   Decimo_12.innerHTML= (totalRankN[11]);
+   Decimo_13.innerHTML= (totalRankN[12]);
+   Decimo_14.innerHTML= (totalRankN[13]);
+   Decimo_15.innerHTML= (totalRankN[14]);
+
+   Rank1.innerHTML= (totalRankS[0]); 
+   Rank2.innerHTML= (totalRankS[1]);
+   Rank3.innerHTML= (totalRankS[2]);
+   Rank4.innerHTML= (totalRankS[3]);
+   Rank5.innerHTML= (totalRankS[4]);
+   Rank6.innerHTML= (totalRankS[5]);
+   Rank7.innerHTML= (totalRankS[6]);
+   Rank8.innerHTML= (totalRankS[7]);
+   Rank9.innerHTML= (totalRankS[8]);
+   Rank10.innerHTML= (totalRankS[9]);
+   Rank11.innerHTML= (totalRankS[10]);
+   Rank12.innerHTML= (totalRankS[11]);
+   Rank13.innerHTML= (totalRankS[12]);
+   Rank14.innerHTML= (totalRankS[13]);
+   Rank15.innerHTML= (totalRankS[14]);
+
 
     });
     
@@ -175,6 +268,8 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("press_rank");
         nav.classList.remove("press_home");
         nav.classList.remove("press_game");
+        SeccionRank.classList.remove("Play_rank");
+
         nav.classList.add("press");
     });
 
@@ -182,7 +277,11 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("press_rank");
         nav.classList.remove("press");
         nav.classList.remove("press_game");
+        SeccionRank.classList.remove("Play_rank");
+
         nav.classList.add("press_home");
+        Playgame.classList.add("play");
+        home.classList.add("play");
 
     });
     
@@ -191,6 +290,7 @@ document.addEventListener("DOMContentLoaded", function() {
         nav.classList.remove("press_rank");
         nav.classList.remove("press");
         nav.classList.remove("press_home");
+        SeccionRank.classList.remove("Play_rank");
         nav.classList.add("press_game");
     });
 
