e74c5f74c7636fc67bfbff1a5e23570a7469d6ae
Mary16RB <162063615+Mary16RB@users.noreply.github.com>
2024-06-28 16:46:41 -0600

    juego 2

--- juego/come_Apple.js
+++ juego/come_Apple.js
@@ -7,7 +7,8 @@ var config = {
   width: 590,
   height: 404,
   parent: 'juego',
-  scene:[{preload: preload,
+  scene:[{
+         preload: preload,
          create: create,
          update: update,
          extend:{
@@ -23,12 +24,16 @@ var config = {
       gravity: { y: gravedad },
       debug: false
     }
-  }
+  },
+  fps: {
+    target: 60, // Limita a 30 FPS
+    forceSetTimeOut: true // Usar setTimeout en lugar de requestAnimationFrame
+}
 }
 
 var game = new Phaser.Game(config);
-
-
+var delayApple=4000;
+var delayPacas=Phaser.Math.Between(1000, 3000);
 var App=this.apple;
 var fondo1;
 var estado=true;
@@ -55,17 +60,24 @@ var ticketText=this.tichetText;
 var dist;
 var cae;
 var estadoCae=0;
-var mouseIsPressed = false;
+var mouseIsPressed;
 var wasSpace;
 var spaceIs;
-var wasMause;
+var wasMause=false;
 var cont=0;
+var contM=0;
 
 function preload() {
   
   this.load.image('background', 'assets/fondo_doble.png');
-  this.load.spritesheet('piggie', 'assets/Piggies_unidos.png',{frameWidth: 74, frameHeight:92});
+  this.load.spritesheet('piggie1', 'assets/Piggies1.png',{frameWidth: 68, frameHeight:92});
+  
+  this.load.spritesheet('piggie2', 'assets/Piggies2.png',{frameWidth: 68, frameHeight:92});
+  this.load.spritesheet('piggie3', 'assets/Piggies3.png',{frameWidth: 68, frameHeight:92});
+  this.load.spritesheet('piggie4', 'assets/Piggies4.png',{frameWidth: 68, frameHeight:92});
+  this.load.spritesheet('piggie5', 'assets/Piggies5.png',{frameWidth: 68, frameHeight:92});
   this.load.image('piso', 'assets/linea_suleo.png');
+
   this.load.image('apple', 'assets/apple.png');
   this.load.image('paca', 'assets/Paca_paja.png');
   this.load.spritesheet('vida','assets/vidas.png',{frameWidth: 143, frameHeight:47});
@@ -74,6 +86,9 @@ function preload() {
   this.load.image('fondoInicio', 'assets/Backgrawn.png');
   this.load.image('gameover','assets/game_over.png');
   this.load.spritesheet('play','assets/boton_play.png',{frameWidth: 159, frameHeight:61});
+
+  this.load.audio('eatSoud', 'audios/eating-sound-effect-36186.mp3');
+  this.load.audio('downSoud', 'audios/085735_pig-86586.mp3');
   this.cont=0;
 
 }
@@ -87,9 +102,34 @@ function create(){
   fondo1 = this.add.tileSprite(0, 0, config.width, config.height, 'background');
   fondo1.setOrigin(0, 0);
  
-  this.piggie= this.physics.add.sprite(150, 321, 'piggie');
+  this.piggie= this.physics.add.sprite(150, 321, 'piggie1');
   this.piggie.body.setSize(40, 60); // Establece el tamaño alrededor de la silueta
-  this.piggie.body.setOffset(17, 32);
+  this.piggie.body.setOffset(17, 30);
+  
+
+  this.piggie2= this.physics.add.sprite(150, 321, 'piggie2');
+  this.piggie2.body.setSize(40, 60); // Establece el tamaño alrededor de la silueta
+  this.piggie2.body.setOffset(17, 32);
+  
+  this.piggie2.disableBody(true,true);
+
+  this.soundEat = this.sound.add('eatSoud');
+  this.soundDown = this.sound.add('downSoud');
+/*
+  this.piggie3= this.physics.add.sprite(150, 321, 'piggie3');
+  this.piggie3.body.setSize(40, 60); // Establece el tamaño alrededor de la silueta
+  this.piggie3.body.setOffset(17, 32);
+  this.piggie3.body.setVisible(false);
+
+  this.piggie4= this.physics.add.sprite(150, 321, 'piggie4');
+  this.piggie4.body.setSize(40, 60); // Establece el tamaño alrededor de la silueta
+  this.piggie4.body.setOffset(17, 32);
+  this.piggie4.body.setVisible(false);
+
+  this.piggie5= this.physics.add.sprite(150, 321, 'piggie5');
+  this.piggie5.body.setSize(40, 60); // Establece el tamaño alrededor de la silueta
+  this.piggie5.body.setOffset(17, 32);
+  this.piggie5.body.setVisible(false);*/
   
   this.piso= this.physics.add.image(295, 390, 'piso').setImmovable();
   App= this.physics.add.image(600,352, 'apple');
@@ -100,7 +140,7 @@ function create(){
   fondoPierde.setOrigin(0, 0);
   fondoPierde.setVisible(false);
 
-  vida= this.add.sprite(90 ,30,'vada');
+  vida= this.add.sprite(90 ,30,'vida');
   this.contador=this.add.image(520,30,'contador');
   scoretext = this.add.bitmapText(485, 24, 'myFont', '0', 18); 
   ticketText = this.add.bitmapText(230, 290, 'myFont', 'Tichets: 0', 18); 
@@ -111,31 +151,63 @@ function create(){
   Play = this.add.sprite(300,360 ,'play').setInteractive();
   
 
-  this.piso.body.allowGravity=false;
+  this.piso.body.allowGravity=false; 
   this.physics.add.collider(pacas, this.piso);
   this.physics.add.collider(App, this.piso);
   this.physics.add.collider(this.piggie, this.piso);
-
-   
+  this.physics.add.collider(this.piggie2, this.piso);
+  
 
   this.piso.setBounce(0);
+  
   this.piggie.setBounce(0);
+  this.piggie2.setBounce(0);/*
+  this.piggie3.setBounce(0);
+  this.piggie4.setBounce(0);
+  this.piggie5.setBounce(0);*/
   App.setBounce(0);
  
 
   pacas.setVelocityX(obspeed);
-
+ 
    Pig=this.piggie;
-   this.input.on('pointerdown', function () {
+   
+  /*this.input.on('pointerdown', function () {
     wasMause=mouseIsPressed;
     mouseIsPressed = true;
+    contM++;
     
+    Pig.anims.play(salto);
+
+    
+    
+    console.log(Pig.y);
+
+    if(contM==1){
+      Pig.setVelocityY(-500);
+       this.saltar();
+
+      console.log(gravedad);
+      this.cont=2;
+    }
+
+    if(contM==2){
+     
+      Pig.y=300;
+      this.saltar();
+      
+    this.cont=1;
+    }
+   console.log("contM "+contM);
+
+
 });
 
 this.input.on('pointerup', function () {
   mouseIsPressed = false;
-  wasMause=mouseIsPressed;
-});
+  
+  
+})*/
 
   Play.on('pointerover', ()=>{
     Play.setFrame(1);
@@ -149,18 +221,20 @@ this.input.on('pointerup', function () {
    vidasText = this.add.text(30, 50, 'Vidas: 3', { fontSize: '16px', fill: '#000' });
 
    function colectar( App, Pig){
+
+    this.soundEat.play();
     App.disableBody(true, true);
     score += 0.5;
     scoretext.setText(score);
-    
 
 }
 
   function pierde(pacas, Pig){
+
+    this.soundDown.play();
     console.log('entro');
    this.physics.pause();
    
-
     vidas -= 0.5;
     vidasText.setText('Vidas: ' + vidas);
     
@@ -190,96 +264,99 @@ this.input.on('pointerup', function () {
   
   }
     
-  this.physics.add.overlap(App, Pig, colectar, colectar, this);
-  this.physics.add.overlap(pacas, Pig, pierde, pierde, this);
+  this.physics.add.overlap(App, this.piggie, colectar, colectar, this);
+  this.physics.add.overlap(pacas, this.piggie, pierde, pierde, this);
 
-   
+  this.physics.add.overlap(App, this.piggie2, colectar, colectar, this);
+  this.physics.add.overlap(pacas, this.piggie2, pierde, pierde, this);
+
+
   this.anims.create({
     key: 'Easy',
-    frames: this.anims.generateFrameNumbers('piggie', { start: 0, end: 6 }),
+    frames: this.anims.generateFrameNumbers('piggie1', { start: 0, end: 6 }),
     frameRate: 20
 
 });
 this.anims.create({
   key: 'Normal',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 12, end: 18 }),
+  frames: this.anims.generateFrameNumbers('piggie2', { start: 0, end: 6 }),
   frameRate: 20
   
 });
 this.anims.create({
   key: 'Dificil',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 24, end: 30 }),
+  frames: this.anims.generateFrameNumbers('piggie3', { start: 0, end: 6 }),
   frameRate: 20
   
 });
 this.anims.create({
   key: 'legend',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 36, end: 42 }),
+  frames: this.anims.generateFrameNumbers('piggie4', { start: 0, end: 6 }),
   frameRate: 20
   
 });
 this.anims.create({
   key: 'Mortal',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 48, end: 54 }),
+  frames: this.anims.generateFrameNumbers('piggie5', { start: 0, end: 6 }),
   frameRate: 20
   
 });
 
 this.anims.create({
   key: 'cae1',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 7, end: 11 }),
+  frames: [ { key: 'piggie1', frame: 7 } ],
   frameRate: 20
   
 });
 this.anims.create({
   key: 'cae2',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 19, end: 23 }),
+  frames: [ { key: 'piggie2', frame: 7 } ],
   frameRate: 20
   
 });
 this.anims.create({
   key: 'cae3',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 31, end: 35 }),
+  frames: [ { key: 'piggie3', frame: 7 } ],
   frameRate: 20
   
 });
 this.anims.create({
   key: 'cae4',
-  frames: this.anims.generateFrameNumbers('piggie', { start:43 , end: 47 }),
+  frames: [ { key: 'piggie4', frame: 7 } ],
   frameRate: 20
   
 });
 this.anims.create({
   key: 'cae5',
-  frames: this.anims.generateFrameNumbers('piggie', { start: 55, end: 59 }),
+  frames: [ { key: 'piggie5', frame: 7 } ],
   frameRate: 20
   
 });
 
 this.anims.create({
     key: 'salto1',
-    frames: [ { key: 'piggie', frame: 5 } ],
+    frames: [ { key: 'piggie1', frame: 5 } ],
     frameRate: 20
 });
 this.anims.create({
   key: 'salto2',
-  frames: [ { key: 'piggie', frame: 17 } ],
+  frames: [ { key: 'piggie2', frame: 5 } ],
   frameRate: 20
 });
 
 this.anims.create({
   key: 'salto3',
-  frames: [ { key: 'piggie', frame: 29 } ],
+  frames: [ { key: 'piggie3', frame: 5 } ],
   frameRate: 20
 });
 this.anims.create({
   key: 'salto4',
-  frames: [ { key: 'piggie', frame: 41 } ],
+  frames: [ { key: 'piggie4', frame: 5 } ],
   frameRate: 20
 });
 this.anims.create({
   key: 'salto5',
-  frames: [ { key: 'piggie', frame: 53 } ],
+  frames: [ { key: 'piggie5', frame: 5 } ],
   frameRate: 20
 });
    this.cursors= this.input.keyboard.createCursorKeys();
@@ -306,7 +383,7 @@ this.anims.create({
 });
 
    this.time.addEvent({
-        delay: 2000,
+        delay:Phaser.Math.Between(2000, delayApple),
         loop: true,
         callback:() =>{
           this.generarApple()
@@ -321,9 +398,9 @@ this.anims.create({
    App.setVisible(true);
    }
   
-
+   
    this.time.addEvent({
-        delay: Phaser.Math.Between(1000, 3000), // De 1 a 3 segundos
+        delay: delayPacas, // De 1 a 3 segundos
         loop: true,
         callback: function() {
             // Generar un número aleatorio entre 1 y 3 para la cantidad de objetos a crear
@@ -341,6 +418,11 @@ this.anims.create({
                  
 
                   this.physics.add.overlap(pacas, Pig, pierde, pierde, this);
+                  /*
+                  this.physics.add.overlap(pacas, this.piggie2, pierde, pierde, this);
+                  this.physics.add.overlap(pacas, this.piggie3, pierde, this);
+                  this.physics.add.overlap(pacas, this.piggie4, pierde, pierde, this);
+                  this.physics.add.overlap(pacas, this.piggie5, pierde, pierde, this);*/
                   this.physics.add.overlap(App, pacas, distancia, distancia, this);
 
 
@@ -382,8 +464,24 @@ function update(){
   wasSpace=spaceIs;
   spaceIs=this.cursors.space.isDown;
 
+  
+    wasMause=mouseIsPressed;
+    mouseIsPressed = this.input.activePointer.isDown;
+
+    if(mouseIsPressed && !wasMause){
+    contM=1;
+    }
+   
+   console.log("contM "+contM);
+  contM=0;
+
+
+
+  console.log("mouse "+mouseIsPressed+" "+wasMause);
+  Pig=this.piggie;
+
   if(estadoCae==1){
-    this.piggie.anims.play(cae);
+    Pig.anims.play(cae);
   }
   switch (true) {
     case (score>= 8) && (score< 15):
@@ -393,6 +491,12 @@ function update(){
       speed=13;
       obspeed=-750;
       delay=90;
+      delayApple=4000;
+      /*this.piggie2.body.setVisible(true);
+      this.piggie.body.setVisible(false);
+      this.piggie3.body.setVisible(false);
+      this.piggie4.body.setVisible(false);
+      this.piggie5.body.setVisible(false);*/
         break;
     case (score >= 15 &&score < 25):
       nivel='Dificil';
@@ -401,6 +505,13 @@ function update(){
       speed=15;
       obspeed=-850;
       delay=80;
+      delayPacas=Phaser.Math.Between(1000, 2500);
+      delayApple=5000;
+      /*this.piggie.body.setVisible(false);
+      this.piggie2.body.setVisible(false);
+      this.piggie3.body.setVisible(true);
+      this.piggie4.body.setVisible(false);
+      this.piggie5.body.setVisible(false);*/
         break;
     case ((score >=25 && score < 30)):
      nivel='legend';
@@ -409,6 +520,13 @@ function update(){
      speed=20;
      obspeed=-1000;
      delay=50;
+     delayPacas=Phaser.Math.Between(500, 2500);
+     delayApple=6000;
+     /*this.piggie.body.setVisible(false);
+      this.piggie2.body.setVisible(false);
+      this.piggie3.body.setVisible(false);
+      this.piggie4.body.setVisible(true);
+      this.piggie5.body.setVisible(false);*/
         break;
   
     case ((score>=30)):
@@ -418,6 +536,7 @@ function update(){
      speed=25;
      obspeed=-1200;
      delay=20;
+     delayPacas=Phaser.Math.Between(300, 1000);
         break;
     default:
       nivel='Easy';
@@ -427,15 +546,11 @@ function update(){
       obspeed=-550;
       delay=120;
       
+      
         // Código para cualquier otro caso
         break;
   }
-  if (!isPaused) {
-    // Incrementa la posición solo si no está pausado
-    fondo1.tilePositionX += speed;
-  }
   
-
   switch (vidas) {
     case 3:
        vida.anims.play('3vidas');
@@ -452,21 +567,23 @@ function update(){
         // Código para cualquier otro caso
         break;
 }
-
+if (!isPaused) {
+  // Incrementa la posición solo si no está pausado
+  fondo1.tilePositionX += speed;
+}
 
   if (fondo1.tilePositionX >= 2*fondo1.width) {
       fondo1.tilePositionX=0;
   }
 
-  if((( spaceIs && !wasSpace)&&(this.cont<=1))||((mouseIsPressed && (wasMause==false) &&(this.cont<=1)))){
-    this.piggie.anims.play(salto);
+  if((( spaceIs && !wasSpace)&&(this.cont<=1))||(mouseIsPressed && !wasMause)&&(this.cont<=1)){
 
+    Pig.anims.play(salto);  
     
-    
-    console.log(this.piggie.y);
+    console.log(Pig.y);
 
     if(this.cont==1){
-      this.piggie.setVelocityY(-500);
+      Pig.setVelocityY(-500);
        this.saltar();
 
       console.log(gravedad);
@@ -475,7 +592,7 @@ function update(){
 
     if(this.cont==0){
      
-      this.piggie.y=300;
+      Pig.y=300;
       this.saltar();
       
     this.cont=1;
@@ -495,9 +612,10 @@ function update(){
   //this.cont=2;
   
 //}
-if((this.piggie.y>=330)){
- this.piggie.anims.play(nivel,true); 
+if((Pig.y>=330)){
+ Pig.anims.play(nivel,true); 
  this.cont=0;
+ contM=0;
  gravedad=1000;
 }
 break;
@@ -591,8 +709,9 @@ function generarApple(){
  App.x=dist;
  App.setVisible(true);
  }
+
 function saltar(){
-  this.piggie.setVelocityY(-300);
+  Pig.setVelocityY(-300);
   this.time.addEvent({
     delay:200,
     loop:false,
@@ -600,21 +719,22 @@ function saltar(){
       if(cont==0){
       gravedad=6000;
       console.log(gravedad);
-      this.piggie.setVelocityY(0);
+      Pig.setVelocityY(0);
       }
       if(cont>=1){
         gravedad=8000;
         console.log(gravedad);
-        this.piggie.setVelocityY(0);
+        Pig.setVelocityY(0);
       }
     }
-  })
+  });
 }
 
 function updateScoreInFirebase() {
 
   console.log('Nuevo puntaje:', score);
   sessionStorage.setItem('Puntaje', score);
+  //window.postMessage({ type: 'updateScore', score: score }, '*');
 
 
 /*
