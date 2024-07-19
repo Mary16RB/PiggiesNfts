1059b9f0bf971981ecd4de6434ba473694a8c888
Mary16RB <162063615+Mary16RB@users.noreply.github.com>
2024-07-19 15:47:38 -0600

    velo3

--- juego/come_Apple.js
+++ juego/come_Apple.js
@@ -401,7 +401,7 @@ this.anims.create({
   
    
    this.time.addEvent({
-        delay: 1300, // De 1 a 3 segundos
+        delay: delayPacas, // De 1 a 3 segundos
         loop: true,
         callback: function() {
             // Generar un número aleatorio entre 1 y 3 para la cantidad de objetos a crear
@@ -506,7 +506,7 @@ function update(){
       speed=20;
       obspeed=-1000;
       delay=80;
-      delayPacas=Phaser.Math.Between(1000, 2000);
+      delayPacas=Phaser.Math.Between(1000, 1500);
       delayinicio=2000;
       delayApple=5000;
       /*this.piggie.body.setVisible(false);
@@ -538,7 +538,7 @@ function update(){
      speed=20;
      obspeed=-2000;
      delay=20;
-     delayPacas=Phaser.Math.Between(500, 1000);
+     delayPacas=Phaser.Math.Between(1000, 1100);
         break;
     default:
       nivel='Easy';
